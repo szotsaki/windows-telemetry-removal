c7f4c805d8ba9fd62ba723c507a683cf4b74b9bf
Szőts Ákos <szotsaki@gmail.com>
2015-09-13 16:20:57 +0200

    First commit
    First version of the script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# windows-telemetry-removal
-Scripts which make turning off Windows telemetry easier
+# Windows telemetry removal
+Run this script with Administrator privileges to remove user habits and diagnostic data reporting to Microsoft.
+
+The script is based on information available at https://redd.it/3g7hr0
---
+++ WindowsTelemetryRemoval.bat
@@ -0,0 +1,51 @@
+@echo off
+
+echo Uninstalling Update for customer experience and diagnostic telemetry (KB3068708)
+wusa /uninstall /kb:3068708 /quiet /norestart > NUL
+
+echo Uninstalling Update for customer experience and diagnostic telemetry (KB3022345)
+wusa /uninstall /kb:3022345 /quiet /norestart > NUL
+
+echo Uninstalling Compatibility update for upgrading Windows 7 (KB2952664)
+wusa /uninstall /kb:2952664 /quiet /norestart > NUL
+
+echo Uninstalling Update that enables you to upgrade from Windows 7 to a later version of Windows (KB2990214)
+wusa /uninstall /kb:2990214 /quiet /norestart > NUL
+
+echo Uninstalling Update installs Get Windows 10 app in Windows 8.1 and Windows 7 SP1 (KB3035583)
+wusa /uninstall /kb:3035583 /quiet /norestart > NUL
+
+echo Uninstalling Description of the update for Windows Activation Technologies (KB971033)
+wusa /uninstall /kb:971033 /quiet /norestart > NUL
+
+echo Uninstalling Update to Windows 7 SP1 for performance improvements (KB3021917)
+wusa /uninstall /kb:3021917 /quiet /norestart > NUL
+
+echo Uninstalling Update that enables you to upgrade from Windows 8.1 to a later version of Windows (KB3044374)
+wusa /uninstall /kb:3044374 /quiet /norestart > NUL
+
+echo Stopping and deleting Windows Diagnostic Tracking Service
+sc stop Diagtrack > NUL
+sc delete Diagtrack > NUL
+
+echo Disabling data collecting and reporting tasks
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\AitAgent" /DISABLE > NUL
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /DISABLE > NUL
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\ProgramDataUpdater" /DISABLE > NUL
+
+schtasks /Change /TN "\Microsoft\Windows\Autochk\Proxy" /DISABLE > NUL
+
+schtasks /Change /TN "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /DISABLE > NUL
+schtasks /Change /TN "\Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /DISABLE > NUL
+schtasks /Change /TN "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /DISABLE > NUL
+
+schtasks /Change /TN "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /DISABLE > NUL
+
+schtasks /Change /TN "\Microsoft\Windows\Maintenance\WinSAT" /DISABLE > NUL
+
+echo Done.
+echo.
+echo Please go to Control Panel ^> Windows Update and hide the following updates:
+echo KB2952664, KB2990214, KB3021917, KB3022345, KB3035583, KB3044374, KB3068708 and KB971033
+
+pause
